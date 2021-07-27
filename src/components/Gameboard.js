e39aa07c4c78f3a8873ecc79a0405af38c1ca4ba
antelor <ramiro.antelo4@gmail.com>
2021-07-27 14:32:48 -0300

    comentarios en shuffle cartas

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -7,9 +7,11 @@ const Gameboard = ({ cards, clickCard, setCards }) => {
         <div>
             Cancha:
             {cards
+                //shuffle
                 .map((value) => ({ value, sort: Math.random() }))
                 .sort((a, b) => a.sort - b.sort)
                 .map(({ value }) => value)
+                //mapeo para display
                 .map((item, index, arr) =>
                 <Card cid={item.id} cimg={item.img} cclicked={item.clicked} clickCard={clickCard}/>
             )}
